e08fe5e25d76bba1bda4925f488b429c1412a96e
kid-0 <64641404+kid-0@users.noreply.github.com>
2023-01-13 13:07:25 +0100

    added a pokemonList array to the scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-alert('Hello World');
-
-let favoriteFood = 'Pasta';
-document.write(favoriteFood);
+let pokemonList = [
+    { name: 'Charizard', height: 1.7, types: ['fire', 'flying'] },
+    { name: 'Nidoran', height: 0.5, types: ['poison'] },
+    { name: 'Jigglypuff', height: 0.5, types: ['fairy', 'normal'] },
+    { name: 'Golem', height: 1.4, types: ['rock', 'ground'] },
+    { name: 'Exeggutor', height: 2, types: ['psychic', 'grass'] }
+];
